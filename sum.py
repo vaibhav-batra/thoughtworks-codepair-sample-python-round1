56abe8d99105e98c3c0c3c973c3361a8bd5ae162
Vaibhav <vaibhav2793@gmail.com>
2023-05-02 17:35:04 +0530

    example solved

--- sum.py
+++ sum.py
@@ -1,9 +1,11 @@
-import sys
+import sys, os
 
 from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
-
+    
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    
     # create Spark context with necessary configuration
     sc = SparkContext("local","PySpark Sum Exmaple")
 
--- tests/unit/test_mathFunctions.py
+++ tests/unit/test_mathFunctions.py
@@ -8,10 +8,10 @@ def test_square_method():
     assert square_method(2) == 4
 
 #TODO: Disable pytest.mark.skip by commenting it
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_cube_method():
     assert cube_method(2) == 8
 
 #TODO: Correct the Assertion Logic
 def test_sum_method():
-    assert sum_method(2,4) == 7
+    assert sum_method(2,5) == 7
